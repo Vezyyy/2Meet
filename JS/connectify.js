45f2587871d49d7a95859765fe34b2fa48fc963c
Vezyy <108421377+Vezyyy@users.noreply.github.com>
2024-07-25 15:03:21 +0200

    Main Page Update
    Vezyy

---
+++ JS/connectify.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const navToggle = document.querySelector('.nav-toggle');
+    const navMenu = document.querySelector('.nav-menu');
+
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.toggle('active');
+    });
+});

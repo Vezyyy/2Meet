90ac60ee31a936194e2f83bda6e9d11fa5d730e6
Vezyy <108421377+Vezyyy@users.noreply.github.com>
2024-07-26 13:25:30 +0200

    Vezyy 26.07.2024
    Add Web Loader, Add Post Web, Optymalize CSS, HTML, JS Code.

--- JS/2Meet.js
+++ JS/2Meet.js
@@ -1,3 +1,7 @@
+/* ******************************************************************************************* */
+/* Mobile Nav Script */
+/* ******************************************************************************************* */
+
 document.addEventListener('DOMContentLoaded', () => {
     const navToggle = document.querySelector('.nav-toggle');
     const navMenu = document.querySelector('.nav-menu');
@@ -5,4 +9,17 @@ document.addEventListener('DOMContentLoaded', () => {
     navToggle.addEventListener('click', () => {
         navMenu.classList.toggle('active');
     });
+});
+
+/* ******************************************************************************************* */
+/* Loader Script */
+/* ******************************************************************************************* */
+
+window.addEventListener('load', function () {
+    const preloader = document.getElementById('preloader');
+    preloader.style.opacity = '0';
+    preloader.style.transition = 'opacity 0.5s ease-out';
+    setTimeout(() => {
+        preloader.style.display = 'none';
+    }, 500); // Dopasuj czas do długości animacji
 });
